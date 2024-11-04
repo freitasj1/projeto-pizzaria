f8d45510690b0e3e7dabe4e9c78d674db689329a
freitasj1 <joaopedromacielfreitas@outlook.com>
2024-11-04 18:04:07 -0300

    cabeçalho do site

--- templates/header.php
+++ templates/header.php
@@ -8,10 +8,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+    <link rel="stylesheet" href="css\style.css">
     <link rel="stylesheet" href="css\styles.css">
 </head>
 <body>
     <header>
-        <p>Cabeçalho</p>
-    </header>
+    <nav class="navbar navbar-expand-lg">
+      <a href="index.php" class="navbar-brand">
+        <img src="img/pizza.svg" alt="Pizzaria do João" id="brand-logo">
+      </a>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a href="index.php" class="nav-link">Peça sua pizza</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
