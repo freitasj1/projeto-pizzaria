fd883873f63cd74dd9ab6884d714e5beafc614d3
freitasj1 <joaopedromacielfreitas@outlook.com>
2024-11-04 17:35:07 -0300

    criação de pastas e separação dos templates

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+include_once("templates/header.php");
+?>
+    <h1>Testando css</h1>
+    <i class="fas fa-sync-alt"></i>
+    
+<?php
+include_once("templates/footer.php");
+?>    
---
+++ templates/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faça seu pedido!</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="css\styles.css">
+</head>
+<body>
+    <header>
+        <p>Cabeçalho</p>
+    </header>
