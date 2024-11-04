be205362eb099cfdcb9700da9c6ec33d9d76442a
freitasj1 <joaopedromacielfreitas@outlook.com>
2024-11-04 18:09:21 -0300

    footer do site

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
 include_once("templates/header.php");
 ?>
-    <h1>Testando css</h1>
-    <i class="fas fa-sync-alt"></i>
+    <h1>Dashboard</h1>
+    <!-- <i class="fas fa-sync-alt"></i> -->
     
 <?php
 include_once("templates/footer.php");
