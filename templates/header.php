160a7316eb1a9aeeb95064694c29b00bbcacf1e5
freitasj1 <joaopedromacielfreitas@outlook.com>
2024-11-08 17:40:08 -0300

    arquivos process

--- templates/header.php
+++ templates/header.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="css\style.css">
+    <link rel="stylesheet" href="css\styles.css">
 </head>
 <body>
     <header>
@@ -25,3 +25,6 @@
       </div>
     </nav>
   </header>
+  <div class="alert alert-sucess">
+    <p>Pedido feito com sucesso!</p>
+  </div>
